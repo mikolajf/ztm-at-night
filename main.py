e79e42c29c7ff5448ec12804bbedd8312b567350
mikolajf <mikolaj.fido@gmail.com>
2019-04-09 23:02:11 +0200

    eod

--- main.py
+++ main.py
@@ -55,20 +55,9 @@ def processWK(f,numer_lini):
             break 
     return False 
            
-    
-def readlines(f,name):
-    """ Iterate lines from an archive member. """
-    linesep = os.linesep[-1]
-    line = ''
-    for ch in f.bytestream(name):
-        line += ch
-        if ch == linesep:
-            yield line.decode("cp1250").encode('utf-8')
-            line = ''
-    if line: yield line.decode("cp1250").encode('utf-8')
 
 
-def parse_file(directory,filename):
+def parse_file(filename):
     with open(filename,"r") as f:
         fo = f.readlines()
     
@@ -83,81 +72,9 @@ def parse_file(directory,filename):
         if retcode:
             break
 
-def download_now():
-    import datetime
-    import urllib.request as urllib
-
-    now = datetime.datetime.now()
-    now_str = now.strftime("%y%m%d")
-
-    url = "ftp://rozklady.ztm.waw.pl/RA" + now_str + ".7z"
-
-    try:
-        urllib.urlretrieve(url, "archive.7z")
-    except Exception as e:
-        print(str(e))
-        
-def unzip_download(file_name = "archive.7z"):
-    import os, subprocess
-
-    cwd = os.getcwd()
-    file_path = os.path.join(cwd,file_name)
-    
-    print(file_path)
-
-    subprocess.call(["C:/Program Files/7-Zip/7z.exe", 'e', "-o" + cwd, file_path])
-    
-    os.remove(file_path)
-    print("File Removed!")
 
 
 
-class SevenZFile(object):
-
-
-
-    @classmethod
-    def is_7zfile(cls, filepath):
-        """ Determine if filepath points to a valid 7z archive. """
-        is7z = False
-        fp = None
-        try:
-            fp = open(filepath, 'rb')
-            #archive = py7zlib.Archive7z(fp)
-            is7z = True
-        finally:
-            if fp: fp.close()
-        return is7z
-
-    def __init__(self, filepath):
-        fp = open(filepath, 'rb')
-        self.filepath = filepath
-        self.archive = py7zlib.Archive7z(fp)
-
-    def __contains__(self, name):
-        return name in self.archive.getnames()
-
-    def bytestream(self, name):
-        """ Iterate stream of bytes from an archive member. """
-        if name not in self:
-            raise SevenZFileError('member %s not found in %s' %
-                                  (name, self.filepath))
-        else:
-            member = self.archive.getmember(name)
-            for byte in member.read():
-                if not byte: break
-                yield byte
-
-    def readlines(self, name):
-        """ Iterate lines from an archive member. """
-        linesep = os.linesep[-1]
-        line = ''
-        for ch in self.bytestream(name):
-            line += ch
-            if ch == linesep:
-                yield line.decode("cp1250").encode('utf-8')
-                line = ''
-        if line: yield line.decode("cp1250").encode('utf-8')
 
 
 def download_7zfile(new = False):
@@ -190,10 +107,40 @@ def download_7zfile(new = False):
 
 	return(latest_file)
 
+def readlines(name):
+    # Iterate lines
+    linesep = os.linesep[-1]
+    line = ''
+    for ch in self.bytestream(name):
+        line += ch
+        if ch == linesep:
+            yield line.decode("cp1250").encode('utf-8')
+            line = ''
+    if line: yield line.decode("cp1250").encode('utf-8')
+
+
 if __name__ == "__main__": 
-    latest_file = download_7zfile()
+    # check for existing files or append a new one
+    latest_file = download_7zfile(new = False)
     print(latest_file)
+
+    # unzip the file using system cmd 7z
     file_path = os.path.join(os.getcwd(),'files',latest_file)
-    subprocess.call(["C:/Program Files/7-Zip/7z.exe", 'e', "-o" + os.path.join(os.getcwd(),'files'), file_path])
+    subprocess.call(["C:/Program Files/7-Zip/7z.exe", 'e', "-o" + os.path.join(os.getcwd(),'files'), file_path, "-aos"])
+
+    with open(file_path[:-2] + "txt","r") as f:
+        fo = f.readlines()
+        
+        retcode = False
+        for line in fo:
+            print(line[1:3])
+            if line[1:3] == "##":
+                break
+            retcode = eval('process' + line[1:3] +'(fo,' + line[4:]+')')
+            if retcode:
+                break
+			
+
+
 	       
 
